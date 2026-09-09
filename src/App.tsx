626dfff8f81b0a1f26f0ae1b7f5aaeb030ee2c9c
Dave Welsh <dave@qccareerschool.com>
2026-09-09 11:22:38 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import type { FC } from 'react';
+import { Counter } from './Counter';
+
+const App: FC = () => (
+  <Counter initialCount={3} />
+)
+
+export default App;
---
+++ src/Counter.tsx
@@ -0,0 +1,14 @@
+/**
+ * Create a React component named `Counter` with two optional props of type
+ * `number`: `initialCount` and `increment`.
+ * 
+ * The component should display the current count and a button.
+ * 
+ * The current count should initially be the value of `initialCount`. If
+ * `initialCount` is not provided, the current count should initially be `0`.
+ * 
+ * Each time the button is clicked, add the value of `increment` to the
+ * current count. If `increment` is not provided, add `1`.
+ *
+ * The layout and styling are not being judged.
+ */
